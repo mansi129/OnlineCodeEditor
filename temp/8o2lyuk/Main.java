01bb7d40f75092cc3421bd76ff7e3adda4d7114a
Man <68495518+mansi129@users.noreply.github.com>
2024-03-20 01:34:00 +0530

    Add files via upload

---
+++ temp/8o2lyuk/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+  public static void main(String args[])
+  {
+    int n1 = 288, n2 = 455, sum;
+    sum = n1 + n2;
+    System.out.println("The sum of Numbers is: "+sum );
+  }
+}
